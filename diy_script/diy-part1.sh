9d886396e2fbcfd2ef55cf523726c732ae6cd977
yaya <tb6750217@gmail.com>
2024-01-26 20:55:46 +0800

    Update diy-part1.sh

--- diy_script/diy-part1.sh
+++ diy_script/diy-part1.sh
@@ -16,11 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# 添加插件源码
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# passwall依赖
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
 # Add a feed source
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
